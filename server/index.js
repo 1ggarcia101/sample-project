9ea4af76d8fe4ee300b51d30e1fb8e20a7066b8d
Elan Adolfo Velazquez Vejar <1ggarcia101@gmail.com>
2022-12-19 12:24:46 -0700

    initial commit/set up client/server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// const express = require('express'); //funtion expression method but does the same thing
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
